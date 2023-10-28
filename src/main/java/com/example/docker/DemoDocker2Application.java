535ace33e672dcc879298cca03104a57f302cd3c
ayoubBelhaouzi <ayoub.belhaouzi@gmail.com>
2023-10-28 02:10:54 +0200

    first commit

---
+++ src/main/java/com/example/docker/DemoDocker2Application.java
@@ -0,0 +1,18 @@
+package com.example.docker;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDocker2Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDocker2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Test");
+	}
+}
